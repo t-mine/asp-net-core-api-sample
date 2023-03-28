04f78c164977ad21164d061a39aa960e096d7045
t-mine <tomonori.mine.biz@gmail.com>
2023-03-28 20:12:17 +0900

    Read

---
+++ BlazorApp/BlazorApp/Server/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BlazorApp.Server.Data;
+using BlazorApp.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public BookController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> ListAsync()
+        {
+            var books = await context.Books.ToListAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetAsync(int id)
+        {
+            var book = await context.Books.SingleOrDefaultAsync(b => b.BookId.Equals(id));
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+    }
+}
